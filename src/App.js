8b9927f850d41e0066eaf209b81ffd2de718a468
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-02-27 12:55:19 +0000

    changed endpoints

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ function App() {
       <BrowserRouter>
         <div >
           <header className='flex shadow-lg items-center justify-between px-10 md:px-32 bg-white p-5 backdrop-blur-md fixed w-screen h-[105px] z-10'>
-            <Link to="/"><img className='w-[150px]' src={logo} alt="Advanced Avionics logo"/></Link>
+            <Link to="/advanced-avionics-website"><img className='w-[150px]' src={logo} alt="Advanced Avionics logo"/></Link>
             <ul className='flex gap-3 md:gap-8 tracking-widest uppercase'>
-              <Link to="/" className='font-md hover:text-[#3294e2] transition-all duration-300 underline'>Home</Link>
-              <Link to="/contact" className='font-md hover:text-[#3294e2] transition-all duration-300 underline'>Contact</Link>
+              <Link to="/advanced-avionics-website" className='font-md hover:text-[#3294e2] transition-all duration-300 underline'>Home</Link>
+              <Link to="/advanced-avionics-website/contact" className='font-md hover:text-[#3294e2] transition-all duration-300 underline'>Contact</Link>
             </ul>
           </header>
           <div className='pt-[105px] min-h-[100vh] flex flex-col'>
             <main>
               <Routes>
-                <Route path="/" element={<HeroImageBackground/>}/>
-                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/advanced-avionics-website" element={<HeroImageBackground/>}/>
+                <Route path="/advanced-avionics-website/contact" element={<Contact/>}/>
               </Routes>
             </main>
             <footer className='mt-auto shadow-inner flex justify-between bg-gradient-to-br from-gray-300 to-gray-400 h-fit w-screen p-5 px-7 text-gray-600 text-sm'>
